a8c291698f3555daa0ef16970e7e58da9d6354f0
mokuda2 <michael.okuda99@gmail.com>
2023-10-20 13:31:16 -0600

    knn

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(vroom)
 library(tidymodels)
 library(embed)
+library(discrim)
+library(naivebayes)
+library(kknn)
 
 amazon_train <- vroom("./STAT\ 348/AmazonEmployeeAccess/train.csv")
 amazon_train$ACTION <- factor(amazon_train$ACTION)
@@ -100,3 +103,146 @@ amazon_final <- amazon_predictions %>%
   select(c(Id, Action))
 
 write.csv(amazon_final, "./STAT\ 348/AmazonEmployeeAccess/penalized_log_reg.csv", row.names = F)
+
+## random forest classification
+rf_model <- rand_forest(mtry = tune(),
+                      min_n = tune(),
+                      trees=1000) %>%
+set_engine("ranger") %>%
+set_mode("classification")
+
+# Create a workflow with model & recipe
+target_encoding_amazon_recipe <- recipe(ACTION~., data=amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
+prep <- prep(target_encoding_amazon_recipe)
+baked_train <- bake(prep, new_data = amazon_train)
+
+amazon_workflow <- workflow() %>%
+  add_recipe(target_encoding_amazon_recipe) %>%
+  add_model(rf_model)
+
+# Set up grid of tuning values
+tuning_grid <- grid_regular(mtry(range=c(1,(ncol(amazon_train) - 1))),
+                            min_n(),
+                            levels = 5) ## L^2 total tuning possibilities
+
+# Set up K-fold CV
+folds <- vfold_cv(amazon_train, v = 5, repeats=1)
+
+CV_results <- amazon_workflow %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc)) #Or leave metrics NULL
+
+# Find best tuning parameters
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+# Finalize workflow and predict
+final_wf <- amazon_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=amazon_train)
+
+amazon_predictions <- final_wf %>%
+  predict(new_data = amazon_test, type="prob")
+
+amazon_predictions$Action <- amazon_predictions$.pred_1
+amazon_predictions$Id <- amazon_test$id
+amazon_final <- amazon_predictions %>%
+  select(c(Id, Action))
+
+write.csv(amazon_final, "./STAT\ 348/AmazonEmployeeAccess/rfclassification.csv", row.names = F)
+
+## naive bayes
+target_encoding_amazon_recipe <- recipe(ACTION~., data=amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
+prep <- prep(target_encoding_amazon_recipe)
+baked_train <- bake(prep, new_data = amazon_train)
+
+nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes") # install discrim library for the naivebayes eng
+
+nb_wf <- workflow() %>%
+  add_recipe(target_encoding_amazon_recipe) %>%
+  add_model(nb_model)
+
+# Tune smoothness and Laplace here
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness(),
+                            levels = 5) ## L^2 total tuning possibilities
+
+# Set up K-fold CV
+folds <- vfold_cv(amazon_train, v = 5, repeats=1)
+
+CV_results <- nb_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc)) #Or leave metrics NULL
+
+# Predict
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+# Finalize workflow and predict
+final_wf <- nb_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=amazon_train)
+
+amazon_predictions <- final_wf %>%
+  predict(new_data = amazon_test, type="prob")
+
+amazon_predictions$Action <- amazon_predictions$.pred_1
+amazon_predictions$Id <- amazon_test$id
+amazon_final <- amazon_predictions %>%
+  select(c(Id, Action))
+
+write.csv(amazon_final, "./STAT\ 348/AmazonEmployeeAccess/naivebayes.csv", row.names = F)
+
+## knn
+target_encoding_amazon_recipe <- recipe(ACTION~., data=amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>%
+  step_normalize()
+prep <- prep(target_encoding_amazon_recipe)
+baked_train <- bake(prep, new_data = amazon_train)
+
+knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
+  set_mode("classification") %>%
+  set_engine("kknn")
+
+knn_wf <- workflow() %>%
+  add_recipe(target_encoding_amazon_recipe) %>%
+  add_model(knn_model)
+
+# Fit or Tune Model HERE
+tuning_grid <- grid_regular(neighbors(),
+                            levels = 5) ## L^2 total tuning possibilities
+
+folds <- vfold_cv(amazon_train, v = 5, repeats=1)
+
+CV_results <- knn_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc))
+
+# Predict
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+# Finalize workflow and predict
+final_wf <- knn_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=amazon_train)
+
+amazon_predictions <- final_wf %>%
+  predict(new_data = amazon_test, type="prob")
+
+amazon_predictions$Action <- amazon_predictions$.pred_1
+amazon_predictions$Id <- amazon_test$id
+amazon_final <- amazon_predictions %>%
+  select(c(Id, Action))
+
+write.csv(amazon_final, "./STAT\ 348/AmazonEmployeeAccess/knn.csv", row.names = F)
