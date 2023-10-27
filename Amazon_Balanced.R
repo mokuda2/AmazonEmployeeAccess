2e74f12107261edcb4948c04fdc01f416910fae0
mokuda2 <michael.okuda99@gmail.com>
2023-10-27 16:02:51 -0600

    rf

--- Amazon_Balanced.R
+++ Amazon_Balanced.R
@@ -197,93 +197,93 @@ write.csv(amazon_final, "naivebayes.csv", row.names = F)
 # prep <- prep(target_encoding_amazon_recipe)
 # baked_train <- bake(prep, new_data = amazon_train)
 
-knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
-  set_mode("classification") %>%
-  set_engine("kknn")
-
-knn_wf <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(knn_model)
-
-# Fit or Tune Model HERE
-tuning_grid <- grid_regular(neighbors(),
-                            levels = 5) ## L^2 total tuning possibilities
-
-folds <- vfold_cv(amazon_train, v = 5, repeats=1)
-
-CV_results <- knn_wf %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics=metric_set(roc_auc))
-
-# Predict
-bestTune <- CV_results %>%
-  select_best("roc_auc")
-
-# Finalize workflow and predict
-final_wf <- knn_wf %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=amazon_train)
-
-amazon_predictions <- final_wf %>%
-  predict(new_data = amazon_test, type="prob")
-
-amazon_predictions$Action <- amazon_predictions$.pred_1
-amazon_predictions$Id <- amazon_test$id
-amazon_final <- amazon_predictions %>%
-  select(c(Id, Action))
-
-write.csv(amazon_final, "knn.csv", row.names = F)
-
-## pca
-# naive bayes
-# target_encoding_amazon_recipe <- recipe(ACTION~., data=amazon_train) %>%
-#   step_mutate_at(all_numeric_predictors(), fn=factor) %>%
-#   step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>%
-#   step_normalize(all_predictors()) %>%
-#   step_pca(all_predictors(), threshold=.9)
-# prep <- prep(target_encoding_amazon_recipe)
-# baked_train <- bake(prep, new_data = amazon_train)
-
-nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
-  set_mode("classification") %>%
-  set_engine("naivebayes") # install discrim library for the naivebayes eng
-
-nb_wf <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(nb_model)
-
-# Tune smoothness and Laplace here
-tuning_grid <- grid_regular(Laplace(),
-                            smoothness(),
-                            levels = 5) ## L^2 total tuning possibilities
-
-# Set up K-fold CV
-folds <- vfold_cv(amazon_train, v = 5, repeats=1)
-
-CV_results <- nb_wf %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics=metric_set(roc_auc)) #Or leave metrics NULL
-
-# Predict
-bestTune <- CV_results %>%
-  select_best("roc_auc")
-
-# Finalize workflow and predict
-final_wf <- nb_wf %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=amazon_train)
-
-amazon_predictions <- final_wf %>%
-  predict(new_data = amazon_test, type="prob")
-
-amazon_predictions$Action <- amazon_predictions$.pred_1
-amazon_predictions$Id <- amazon_test$id
-amazon_final <- amazon_predictions %>%
-  select(c(Id, Action))
-
-write.csv(amazon_final, "pcanaivebayes.csv", row.names = F)
+# knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
+#   set_mode("classification") %>%
+#   set_engine("kknn")
+# 
+# knn_wf <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(knn_model)
+# 
+# # Fit or Tune Model HERE
+# tuning_grid <- grid_regular(neighbors(),
+#                             levels = 5) ## L^2 total tuning possibilities
+# 
+# folds <- vfold_cv(amazon_train, v = 5, repeats=1)
+# 
+# CV_results <- knn_wf %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc))
+# 
+# # Predict
+# bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# # Finalize workflow and predict
+# final_wf <- knn_wf %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=amazon_train)
+# 
+# amazon_predictions <- final_wf %>%
+#   predict(new_data = amazon_test, type="prob")
+# 
+# amazon_predictions$Action <- amazon_predictions$.pred_1
+# amazon_predictions$Id <- amazon_test$id
+# amazon_final <- amazon_predictions %>%
+#   select(c(Id, Action))
+# 
+# write.csv(amazon_final, "knn.csv", row.names = F)
+# 
+# ## pca
+# # naive bayes
+# # target_encoding_amazon_recipe <- recipe(ACTION~., data=amazon_train) %>%
+# #   step_mutate_at(all_numeric_predictors(), fn=factor) %>%
+# #   step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>%
+# #   step_normalize(all_predictors()) %>%
+# #   step_pca(all_predictors(), threshold=.9)
+# # prep <- prep(target_encoding_amazon_recipe)
+# # baked_train <- bake(prep, new_data = amazon_train)
+# 
+# nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
+#   set_mode("classification") %>%
+#   set_engine("naivebayes") # install discrim library for the naivebayes eng
+# 
+# nb_wf <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(nb_model)
+# 
+# # Tune smoothness and Laplace here
+# tuning_grid <- grid_regular(Laplace(),
+#                             smoothness(),
+#                             levels = 5) ## L^2 total tuning possibilities
+# 
+# # Set up K-fold CV
+# folds <- vfold_cv(amazon_train, v = 5, repeats=1)
+# 
+# CV_results <- nb_wf %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc)) #Or leave metrics NULL
+# 
+# # Predict
+# bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# # Finalize workflow and predict
+# final_wf <- nb_wf %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=amazon_train)
+# 
+# amazon_predictions <- final_wf %>%
+#   predict(new_data = amazon_test, type="prob")
+# 
+# amazon_predictions$Action <- amazon_predictions$.pred_1
+# amazon_predictions$Id <- amazon_test$id
+# amazon_final <- amazon_predictions %>%
+#   select(c(Id, Action))
+# 
+# write.csv(amazon_final, "pcanaivebayes.csv", row.names = F)
 
 # knn
 # target_encoding_amazon_recipe <- recipe(ACTION~., data=amazon_train) %>%
@@ -294,43 +294,43 @@ write.csv(amazon_final, "pcanaivebayes.csv", row.names = F)
 # prep <- prep(target_encoding_amazon_recipe)
 # baked_train <- bake(prep, new_data = amazon_train)
 
-knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
-  set_mode("classification") %>%
-  set_engine("kknn")
-
-knn_wf <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(knn_model)
-
-# Fit or Tune Model HERE
-tuning_grid <- grid_regular(neighbors(),
-                            levels = 5) ## L^2 total tuning possibilities
-
-folds <- vfold_cv(amazon_train, v = 5, repeats=1)
-
-CV_results <- knn_wf %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics=metric_set(roc_auc))
-
-# Predict
-bestTune <- CV_results %>%
-  select_best("roc_auc")
-
-# Finalize workflow and predict
-final_wf <- knn_wf %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=amazon_train)
-
-amazon_predictions <- final_wf %>%
-  predict(new_data = amazon_test, type="prob")
-
-amazon_predictions$Action <- amazon_predictions$.pred_1
-amazon_predictions$Id <- amazon_test$id
-amazon_final <- amazon_predictions %>%
-  select(c(Id, Action))
-
-write.csv(amazon_final, "pcaknn.csv", row.names = F)
+# knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
+#   set_mode("classification") %>%
+#   set_engine("kknn")
+# 
+# knn_wf <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(knn_model)
+# 
+# # Fit or Tune Model HERE
+# tuning_grid <- grid_regular(neighbors(),
+#                             levels = 5) ## L^2 total tuning possibilities
+# 
+# folds <- vfold_cv(amazon_train, v = 5, repeats=1)
+# 
+# CV_results <- knn_wf %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc))
+# 
+# # Predict
+# bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# # Finalize workflow and predict
+# final_wf <- knn_wf %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=amazon_train)
+# 
+# amazon_predictions <- final_wf %>%
+#   predict(new_data = amazon_test, type="prob")
+# 
+# amazon_predictions$Action <- amazon_predictions$.pred_1
+# amazon_predictions$Id <- amazon_test$id
+# amazon_final <- amazon_predictions %>%
+#   select(c(Id, Action))
+# 
+# write.csv(amazon_final, "pcaknn.csv", row.names = F)
 
 ## svm
 svmRadial <- svm_rbf(rbf_sigma=tune(), cost=tune()) %>% # set or tune
